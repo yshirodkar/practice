a803957758fc971d71f700c65fb853c75dcc12e0
yogesh <yogesh2490@gmail.com>
2019-01-27 22:12:07 -0700

    Adding level order traversal

---
+++ BinaryTree/LevelOrderTraversal.go
@@ -0,0 +1,30 @@
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func levelOrder(root *TreeNode) [][]int {
+	res := [][]int{}
+	var dfs func(*TreeNode, int)
+
+	dfs = func(root *TreeNode, level int) {
+		if root == nil {
+			return
+		}
+
+		if  level >= len(res)   {
+			res = append(res, []int{})
+		}
+		res[level] = append(res[level], root.Val)
+
+		dfs(root.Left, level+1)
+		dfs(root.Right, level+1)
+	}
+
+	dfs(root, 0)
+
+	return res
+}
