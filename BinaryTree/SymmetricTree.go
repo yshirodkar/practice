948055a02b93fcd9f9f90886449ef6601a16f0ff
Yogesh Shirodkar <yogesh2490@gmail.com>
2019-02-09 15:51:14 -0700

    Adding more leetcode practice

---
+++ BinaryTree/SymmetricTree.go
@@ -0,0 +1,44 @@
+/*
+Given a binary tree, check whether it is a mirror of itself (ie, symmetric around its center).
+For example, this binary tree [1,2,2,3,4,4,3] is symmetric:
+    1
+   / \
+  2   2
+ / \ / \
+3  4 4  3
+But the following [1,2,2,null,3,null,3] is not:
+    1
+   / \
+  2   2
+   \   \
+   3    3
+*/
+
+
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func isSymmetric(root *TreeNode) bool {
+	if root == nil {
+		return true 
+	}
+
+	return recur(root.Left, root.Right)
+}
+
+func recur(left, right *TreeNode) bool {
+	if left == nil && right == nil {
+		return true
+	}
+
+	if left == nil || right == nil {
+		return false
+	}
+
+	return left.Val == right.Val && recur(left.Left, right.Right) && recur(left.Right, right.Left)
+}
