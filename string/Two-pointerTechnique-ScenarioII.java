2caf830ce725f78966530987c548106ad9ce67f2
Yogesh Shirodkar <yogesh2490@gmail.com>
2019-02-15 23:15:08 -0700

    string solution and two pointer solutions

---
+++ string/Two-pointerTechnique-ScenarioII.java
@@ -0,0 +1,13 @@
+/*
+Given an array and a value, remove all instances of that value in-place and return the new length.
+*/
+public int removeElement(int[] nums, int val) {
+    int k = 0;
+    for (int i = 0; i < nums.length; ++i) {
+        if (nums[i] != val) {
+            nums[k] = nums[i];
+            k++;
+        }
+    }
+    return k;
+}
